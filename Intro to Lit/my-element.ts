9dd364a9e3b5e6c277c27ed180b50def39b7affd
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-09 15:12:50 -0600

    Paso 7

--- Intro to Lit/my-element.ts
+++ Intro to Lit/my-element.ts
@@ -1,32 +1,54 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement, state, query} from 'lit/decorators.js';
+
+type ToDoItem = {
+  text: string,
+  completed: boolean
+}
 
 @customElement('todo-list')
 export class ToDoList extends LitElement {
+static styles = css`
+  .completed {
+    text-decoration-line: line-through;
+    color: #777;
+  }
+`;
   @state()
   private _listItems = [
-    { text: 'Dormir', completed: true },
-    { text: 'Comer', completed: false }
+    { text: 'Make to-do list', completed: true },
+    { text: 'Add some styles', completed: false }
   ];
 
   render() {
     return html`
-      <h2>Hacer</h2>
+      <h2>To Do</h2>
       <ul>
-         <!-- TODO: Render list items. -->
         ${this._listItems.map((item) =>
-    html`<li>${item.text}</li>`)}
+          html`
+            <li
+                class=${item.completed ? 'completed' : ''}
+                @click=${() => this.toggleCompleted(item)}>
+              ${item.text}
+            </li>`
+        )}
       </ul>
       <input id="newitem" aria-label="New item">
       <button @click=${this.addToDo}>Add</button>
     `;
   }
-  @query('#newitem')
-input!: HTMLInputElement;
 
-addToDo() {
-  this._listItems = [...this._listItems,
-      {text: this.input.value, completed: false}];
-  this.input.value = '';
-}
+  toggleCompleted(item: ToDoItem) {
+    item.completed = !item.completed;
+    this.requestUpdate();
+  }
+
+  @query('#newitem')
+  input!: HTMLInputElement;
+
+  addToDo() {
+    this._listItems = [...this._listItems,
+        {text: this.input.value, completed: false}];
+    this.input.value = '';
+  }
 }
