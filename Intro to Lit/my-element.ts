ebcbd5a2038815d0ed3e15d4c4a7dedeb3559715
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-09 15:06:43 -0600

    Paso 3

--- Intro to Lit/my-element.ts
+++ Intro to Lit/my-element.ts
@@ -1,11 +1,14 @@
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
+ @property()
+ message: string = 'Hola de nuevo! :p';
+
   render() {
     return html`
-      <p>Hola Mundo! Desde my-element.</p>
+      <p>${this.message}</p>
     `;
   }
 }
