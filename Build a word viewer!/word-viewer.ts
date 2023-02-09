bbba0990a6e1842e26a16159469f434ea9d4762a
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-09 16:08:36 -0600

    Paso 2

--- Build a word viewer!/word-viewer.ts
+++ Build a word viewer!/word-viewer.ts
@@ -1,1 +1,15 @@
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
 
+@customElement('word-viewer')
+class WordViewer extends LitElement {
+  render() {
+    return html`<pre>Un template super expresivo :(!</pre>`
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "word-viewer": WordViewer
+  }
+}
