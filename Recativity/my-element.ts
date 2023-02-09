06b6921fc2bee42813085750f5ecb29935025a31
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-09 15:36:54 -0600

    Paso 2

--- Recativity/my-element.ts
+++ Recativity/my-element.ts
@@ -1,1 +1,23 @@
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 
+@customElement('my-element')
+export class MyElement extends LitElement {
+  @property()
+  result = '';
+
+  flipCoin() {
+    if (Math.random() < 0.5) {
+      this.result = 'Cara';
+    } else {
+      this.result = 'Cruz';
+    }
+  }
+
+  render() {
+    return html`
+      <button @click=${this.flipCoin}>Haz el volado!</button>
+      <p>Resultado del volado: ${this.result}</p>
+    `;
+  }
+}
