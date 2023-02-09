55ac5253e6fa7610840f91677f838169996eb8f1
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-09 15:07:46 -0600

    Paso 4

--- Intro to Lit/my-element.ts
+++ Intro to Lit/my-element.ts
@@ -1,14 +1,20 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
- @property()
- message: string = 'Hola de nuevo! :p';
+@customElement('name-tag')
+export class NameTag extends LitElement {
+  @property()
+  name: string = 'Your name here';
 
   render() {
     return html`
-      <p>${this.message}</p>
+      <p>Hola, ${this.name}</p>
+      <input @input=${this.ChangeName} placeholder="Ingresa tu nombre">
     `;
+  }
+
+  ChangeName(event:Event){
+   const input = event.target as HTMLInputElement;
+    this.name = input.value;
   }
 }
